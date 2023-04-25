9300e89ba6ced8ae45ea440adfaa727c6fb9542c
matpx <9219741+matpx@users.noreply.github.com>
2023-04-25 13:40:12 +0200

    open window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,26 @@
-
+#include <SDL2/SDL.h>
 
 int main() {
+  SDL_Init(SDL_INIT_VIDEO);
+  SDL_Window* window = SDL_CreateWindow("game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1200, 800, SDL_WINDOW_OPENGL);
+  SDL_GLContext context = SDL_GL_CreateContext(window);
+
+  bool running = true;
+  SDL_Event sdl_event;
+
+  while(running) {
+    while(SDL_PollEvent(&sdl_event)) {
+      if(sdl_event.type == SDL_QUIT) {
+        running = false;
+      }
+    }
+
+    SDL_GL_SwapWindow(window);
+  }
+
+  SDL_GL_DeleteContext(context);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
 
   return 0;
 }
