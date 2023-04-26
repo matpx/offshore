9b63cf2c91d605d2eac9ecd99bbd5c5c72c45e67
matpx <9219741+matpx@users.noreply.github.com>
2023-04-26 16:02:51 +0200

    type index

--- src/stb.cpp
+++ src/stb.cpp
@@ -1,4 +1,5 @@
 #define STB_LEAKCHECK_IMPLEMENTATION
+// #define STB_LEAKCHECK_SHOWALL
 #include <stb/stb_leakcheck.h>
 
 #define STB_DS_IMPLEMENTATION
--- src/world.hpp
+++ src/world.hpp
@@ -1,16 +1,35 @@
 #pragma once
 
 #include "types.hpp"
+#include <typeindex>
 
 namespace world {
 
   using EntityId = u32;
 
-  struct Entity {
-    bool valid = true;
-    
+  struct Transform {
     vec3 translation;
     quat rotation;
+  };
+
+  struct Camera {
+    mat4 projection;
+    
+    Camera(float fov, float aspect, float near, float far)
+      : projection(HMM_Perspective_RH_NO(fov, aspect, near, far)) {}
+  };
+
+  struct Entity {
+    bool valid = true;
+    Transform transform;
+
+    std::type_index variant;
+
+    union {
+      Camera camera;
+    };
+
+    Entity(auto component) : variant(typeid(component)), camera(component) {}
   };
 
   EntityId create(const Entity&);
