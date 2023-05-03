18c98e0d1e6b43fdfeaaa0c2aa7e6533ac70ce1f
matpx <9219741+matpx@users.noreply.github.com>
2023-05-03 18:00:20 +0200

    func line

--- src/log.hpp
+++ src/log.hpp
@@ -5,12 +5,12 @@
 
 #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 
-#define FATAL(MSG) { printf("\033[0;31m"); printf("FATAL(%s:%s:%d): ", __FILENAME__, __func__, __LINE__); printf(MSG); printf("\033[0m\n"); assert(false); abort(); }
-#define ERROR(MSG) { printf("\033[0;31m"); printf("ERROR(%s:%s:%d): ", __FILENAME__, __func__, __LINE__); printf(MSG); printf("\033[0m\n"); }
-#define INFO(MSG)  { printf("INFO(%s:%s:%d): ", __FILENAME__, __func__, __LINE__); printf(MSG); printf("\n"); }
+#define FATAL(MSG) { printf("\033[0;31m"); printf("FATAL(%s:%d:%s): ", __FILENAME__, __LINE__, __func__); printf(MSG); printf("\033[0m\n"); assert(false); abort(); }
+#define ERROR(MSG) { printf("\033[0;31m"); printf("ERROR(%s:%d:%s): ", __FILENAME__, __LINE__, __func__); printf(MSG); printf("\033[0m\n"); }
+#define INFO(MSG)  { printf("INFO(%s:%d:%s): ", __FILENAME__, __LINE__, __func__); printf(MSG); printf("\n"); }
 
 #ifndef NDEBUG
-#define DEBUG(MSG) { printf("\033[0;36m"); printf("DEBUG(%s:%s:%d): ", __FILENAME__, __func__, __LINE__); printf(MSG); printf("\033[0m\n"); }
+#define DEBUG(MSG) { printf("\033[0;36m"); printf("DEBUG(%s:%d:%s): ", __FILENAME__, __LINE__, __func__); printf(MSG); printf("\033[0m\n"); }
 #elif
 #define DEBUG(MSG)
 #endif
