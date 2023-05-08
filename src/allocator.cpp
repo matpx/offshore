6c9ce58c54cc59a08219c1afe9a745299edc36df
matpx <9219741+matpx@users.noreply.github.com>
2023-05-08 10:19:39 +0200

    aligned realloc

--- src/allocator.cpp
+++ src/allocator.cpp
@@ -1,5 +1,7 @@
 #include "allocator.hpp"
 
+#include <malloc.h>
+
 #include "types.hpp"
 
 #ifndef NDEBUG
@@ -15,6 +17,26 @@ void* _malloc(size_t size) { return malloc(size); }
 void* _aligned_alloc(size_t alignment, size_t size) { return aligned_alloc(alignment, size); }
 
 void* _realloc(void* ptr, size_t size) { return realloc(ptr, size); }
+
+void* _aligned_realloc(size_t alignment, void* ptr, size_t size) {
+  if (size == 0 && ptr != nullptr) {
+    free(ptr);
+  }
+
+  void* aligned_ptr = _aligned_alloc(alignment, size);
+
+  if (ptr == nullptr) {
+    return aligned_ptr;
+  }
+
+  size_t copy_size = std::min(size, malloc_usable_size(ptr));
+
+  memcpy(aligned_ptr, ptr, copy_size);
+
+  free(ptr);
+
+  return aligned_ptr;
+}
 
 void _free(void* ptr) { free(ptr); }
 
--- src/allocator.hpp
+++ src/allocator.hpp
@@ -9,6 +9,7 @@ constexpr size_t base_alignment = 16;
 void* _malloc(size_t size);
 void* _aligned_alloc(size_t alignment, size_t size);
 void* _realloc(void* ptr, size_t size);
+void* _aligned_realloc(size_t alignment, void* ptr, size_t size);
 void _free(void* ptr);
 void debug_leak_check();
 
