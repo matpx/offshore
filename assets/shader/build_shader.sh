7216f4562bb59c7e4264bea7b323e5f5d33d7410
matpx <9219741+matpx@users.noreply.github.com>
2023-06-12 09:54:13 +0200

    fmt

--- assets/shader/build_shader.sh
+++ assets/shader/build_shader.sh
@@ -4,4 +4,3 @@ cd "$(dirname "$0")"
 
 ../../tools/bin/linux/sokol-shdc -i unlit.glsl -o include/unlit.h --slang=glsl330
 ../../tools/bin/linux/sokol-shdc -i debug.glsl -o include/debug.h --slang=glsl330
-
