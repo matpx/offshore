41ae531dec842fe7a5f516785e5a2245842bbdf8
matpx <9219741+matpx@users.noreply.github.com>
2023-06-13 22:41:09 +0200

    more cmake

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -1,12 +1,12 @@
 #include <SDL2/SDL_timer.h>
 
-#include "./core/input.hpp"
-#include "./gfx/gfx.hpp"
-#include "./gfx/shapes.hpp"
-#include "./gfx/ui.hpp"
-#include "core/log.hpp"
-#include "systems/router.hpp"
-#include "world/world.hpp"
+#include "input.hpp"
+#include "../gfx/gfx.hpp"
+#include "../gfx/shapes.hpp"
+#include "../gfx/ui.hpp"
+#include "log.hpp"
+#include "../systems/router.hpp"
+#include "../world/world.hpp"
 
 int main() {
   LOG_DEBUG("debug mode!");
