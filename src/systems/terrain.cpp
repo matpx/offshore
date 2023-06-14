1fb0046fb48d1b3eef55a1ab33ad42077a8bafce
matpx <9219741+matpx@users.noreply.github.com>
2023-06-14 14:56:12 +0200

    chunk offset

--- src/systems/terrain.cpp
+++ src/systems/terrain.cpp
@@ -3,6 +3,7 @@
 #include <rjm/rjm_mc.h>
 #include <stb/stb_perlin.h>
 
+#include <cassert>
 #include <entt/entity/registry.hpp>
 #include <limits>
 
@@ -17,19 +18,22 @@ static float testIsoFn(const float *position, [[maybe_unused]] float *extra, [[m
   // const vec3 xn = glm::normalize(x) * 2.0f;
   // return glm::length2(x) - 90 - std::max(0.0f, stb_perlin_noise3(xn.x, xn.y, xn.z, 0, 0, 0) * 10.0f);
 
-  const vec3 xv = vec3{position[0], position[1], position[2]} * 2.0f;
+  const vec3 base_offset = *((vec3 *)userparam);
+  const vec3 xv = base_offset + vec3{position[0], position[1], position[2]};
 
-  return std::sin(xv.x) + std::sin(xv.y) + std::sin(xv.z) - 1.5f;
+  return std::sin(glm::length(xv) * 1.0f);
 }
 
-void Terrain::setup() {
-  LOG_DEBUG("terrain create");
+gfx::Mesh build_chunk(const vec3 base_offset) {
+  LOG_DEBUG("chunk create");
 
-  float bmin[3] = {-10, -10, -10};
-  float bmax[3] = {+10, +10, +10};
+  constexpr float radius = 8.0f;
+
+  float bmin[3] = {-radius, -radius, -radius};
+  float bmax[3] = {+radius, +radius, +radius};
   float res = 0.5f;
 
-  McMesh iso_mesh = mcGenerate(bmin, bmax, res, testIsoFn, NULL);
+  McMesh iso_mesh = mcGenerate(bmin, bmax, res, testIsoFn, (void *)&base_offset);
 
   LOG_DEBUG("terrain verts: %d", iso_mesh.nverts);
   LOG_DEBUG("terrain indices: %d", iso_mesh.ntris * 3);
@@ -50,20 +54,27 @@ void Terrain::setup() {
     index_data[n * 3 + 2] = iso_mesh.indices[n * 3 + 2];
   }
 
-  assert(vertex_data.size() > 0 && vertex_data.size() < std::numeric_limits<gfx::index_t>::max());
+  assert(vertex_data.size() > 0);
+  assert(vertex_data.size() < std::numeric_limits<gfx::index_t>::max());
   assert(index_data.size() > 0);
 
   const gfx::Mesh mesh = gfx::create_mesh(vertex_data, index_data);
 
+  mcFree(&iso_mesh);
+
+  return mesh;
+}
+
+void Terrain::setup() {
+  LOG_DEBUG("terrain create");
+
+  const vec3 base_offset = {0, 0, 0};
+
+  const gfx::Mesh mesh = build_chunk(base_offset);
   const entt::entity terrain_chunk = world::registry->create();
 
-  world::registry->emplace<comp::Transform>(terrain_chunk, comp::Transform{});
+  world::registry->emplace<comp::Transform>(terrain_chunk, comp::Transform{.translation = base_offset});
   world::registry->emplace<comp::Renderable>(terrain_chunk, comp::Renderable{mesh, gfx::material::get()});
-
-  vertex_data.clear();
-  index_data.clear();
-
-  mcFree(&iso_mesh);
 }
 
 }  // namespace systems
