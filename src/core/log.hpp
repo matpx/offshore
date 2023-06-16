e68b48a9235eb31a43fae3d90aa2ee09f0e322b4
matpx <9219741+matpx@users.noreply.github.com>
2023-06-16 22:34:12 +0200

    win32 build

--- src/core/log.hpp
+++ src/core/log.hpp
@@ -3,7 +3,11 @@
 #include <cstdio>
 #include <cstring>
 
+#ifdef _WIN32
+#define __FILENAME__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
+#else
 #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+#endif
 
 #define FATAL(...)                                                 \
   {                                                                \
